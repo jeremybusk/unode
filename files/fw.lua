65ab3e9c7cae49c563a752c41da5b899c4b7a615
Jeremy Busk <jeremybusk@gmail.com>
2021-01-19 07:04:20 +0000

    add lua function to return output of command

--- files/fw.lua
+++ files/fw.lua
@@ -1,6 +1,17 @@
+local function osExecute(cmd)
+  local fileHandle     = assert(io.popen(cmd, 'r'))
+  local commandOutput  = assert(fileHandle:read('*a'))
+  local returnTable    = {fileHandle:close()}
+  return commandOutput,returnTable[3]            -- rc[3] contains returnCode
+end
+
 local ipaddr = ngx.var.remote_addr
-local cmd = "sudo /app/unode/bin/fw-update.sh fw-allow-ip " .. ipaddr
-os.execute(cmd)
+local cmd = "sudo /app/unode/bin/fw-update fw-allow-ip " .. ipaddr
+
+r = osExecute(cmd)
+-- r = os.execute(cmd)
+
 ngx.status = ngx.HTTP_OK
 ngx.say("I: ipaddr " .. ipaddr .. " allowed.")
+ngx.say(r)
 return ngx.exit(ngx.HTTP_OK)
