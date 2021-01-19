9b7064c489b380de6e60e935cd1d9119acec2d2a
Jeremy Busk <jeremybusk@gmail.com>
2021-01-19 07:15:41 +0000

    add lua utils file

---
+++ files/utils.lua
@@ -0,0 +1,35 @@
+local utils = {}
+
+
+function utils.rediscon()
+    local redis = require "nginx.redis"
+    local rdb = redis:new()
+    rdb:set_timeout(1000) -- 1 sec
+    local ok, err = rdb:connect("127.0.0.1", 6379)
+    if not ok then
+        ngx.say("failed to connect: ", err)
+        return
+    end
+    local res, err = rdb:auth("redis1in")
+    if not res then
+        ngx.say("failed to authenticate: ", err)
+        return
+    end
+    return rdb
+end
+
+
+function utils.get_header_val(var_name)
+    -- gets value from http headers in format of my_var: somevalue
+    retval = nil
+    local h = ngx.req.get_headers()
+    for k, v in pairs(h) do
+        if k == var_name then
+            retval = v
+        end
+    end
+    return retval
+end
+
+
+return utils
