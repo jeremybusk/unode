1120cb85ab5e53034c6a124bcebfdb42db7d5c8c
Jeremy Busk <jeremybusk@gmail.com>
2021-03-07 19:40:25 -0700

    add initial docker-compose of services

---
+++ docker/README.md
@@ -0,0 +1,4 @@
+reactjs web client for unode
+
+# pgadmin
+- https://www.pgadmin.org/docs/pgadmin4/latest/container_deployment.html
---
+++ docker/db/html/js/config.mjs
@@ -0,0 +1,9 @@
+export function api() {
+var api = {
+  host  : "lxd0.uvoo.io",
+  port  : 13000,
+  front : "#888",
+  side  : "#369"
+};
+return api;
+}
